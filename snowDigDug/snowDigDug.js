d27d15b429ce8a97ebbee22ed7e1738512cb0a07
Miles Leska <37006041+ArmoredGit@users.noreply.github.com>
2019-12-02 15:15:36 -0600

    some comments and two global variables

--- snowDigDug/snowDigDug.js
+++ snowDigDug/snowDigDug.js
@@ -1,8 +1,14 @@
-function setup() {
+var snowflakeArray;
+var snowBlockArray;
+//https://i1.wp.com/www.edcollins.com/digdug/digdug-grid.gif image of dig dug game
 
+function setup() {
+  snowflakeArray = [];
+  snowBlockArray = [];
+  
 }
 
 
 function draw() {
-
+  
 }
