cd3f36649942af747f47ec2602d26b1849f76bb4
Miles Leska <37006041+ArmoredGit@users.noreply.github.com>
2019-12-02 15:28:53 -0600

    setting up setup

--- snowDigDug/snowDigDug.js
+++ snowDigDug/snowDigDug.js
@@ -3,9 +3,19 @@ var snowBlockArray;
 //https://i1.wp.com/www.edcollins.com/digdug/digdug-grid.gif image of dig dug game
 
 function setup() {
+  createCanvas(700,900);
   snowflakeArray = [];
   snowBlockArray = [];
+  for(let i = 0; i < 14;i++){
+    snowBlockArray[i] = [];
+    for(let j = 0; i < 14;i++){
+      snowBlockArray[i][j] = new SnowBlock(i,j);
+    }
+  }
   
+  for(let i = 0; i < 14;i++){
+    snowflakeArray[i] = new Snowflake(random(0,width),0);
+  }
 }
 
 
