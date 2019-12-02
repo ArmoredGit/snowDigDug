ad2a792a9d37c9a47ac44acec9d244f4774479f6
Miles Leska <37006041+ArmoredGit@users.noreply.github.com>
2019-12-02 15:16:03 -0600

    base skeleton of the class

--- snowDigDug/Obstacle.js
+++ snowDigDug/Obstacle.js
@@ -1,1 +1,18 @@
-
+class Obstacle {
+  //Obstacles include the two enemy types and rocks
+  //all Obstacles havwe a move and show
+  //move includes wandering and chasing the player
+  constructor(type,x,y) {
+    this.type = type;
+    this.x = x;
+    this.y = y;
+  }
+  
+  move(){
+    
+  }
+  
+  show(){
+    
+  }
+}
