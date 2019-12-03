9807a692ed4d6c193a9d413f1cfbc65e97d9d415
Miles Leska <37006041+ArmoredGit@users.noreply.github.com>
2019-12-03 11:59:17 -0600

    movement work

--- snowDigDug/Player.js
+++ snowDigDug/Player.js
@@ -6,10 +6,53 @@ class Player  {
   
   show(){
     fill(0);
-    rect((width / 18 * this.x) + (height / 144), (height / 18 * this.y) + (width / 144), (width / 18 * this.x) + (6 * width / 144), (height / 18 * this.y) + (6 * height / 144));
+    stroke(255);
+    rect((width / 18 * this.x) + (height / 144), (3 * height / 18) + (height / 18 * this.y) + (width / 144), (6 * width / 144), (6 * height / 144));
+    stroke(0);
   }
   
   move(direction){
-    
+    //direction is 1 == up,2 == right,3 == down,4 == left
+    if(direction == 1){
+      if(round((this.x * 100) % 100) == 0){
+        this.y -= 0.1;
+      } else {
+        if((this.x * 100) % 100 > 50){
+          this.x += 0.1;
+        } else {
+          this.x += 0.1;
+        }
+      }
+    } else if(direction == 2){
+      if(round((this.y * 100) % 100) == 0){
+        this.x -= 0.1;
+      } else {
+        if((this.y * 100) % 100 > 50){
+          this.y += 0.1;
+        } else {
+          this.y += 0.1;
+        }
+      }
+    } else if(direction == 3){
+      if(round((this.x * 100) % 100) == 0){
+        this.y += 0.1;
+      } else {
+        if((this.x * 100) % 100 > 50){
+          this.x += 0.1;
+        } else {
+          this.x += 0.1;
+        }
+      }
+    } else if(direction == 4){
+      if(round((this.y * 100) % 100) == 0){
+        this.x -= 0.1;
+      } else {
+        if((this.y * 100) % 100 > 50){
+          this.y += 0.1;
+        } else {
+          this.y += 0.1;
+        }
+      }
+    }
   }
 }
