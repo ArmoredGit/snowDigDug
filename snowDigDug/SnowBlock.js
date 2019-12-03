601ca736c2712c1f5cf5e6da04c80631c07307bf
Miles Leska <37006041+ArmoredGit@users.noreply.github.com>
2019-12-02 21:46:23 -0600

    basic snow blocks

--- snowDigDug/SnowBlock.js
+++ snowDigDug/SnowBlock.js
@@ -2,5 +2,25 @@ class SnowBlock {
   constructor(x,y) {
     this.x = x;
     this.y = y;
+    this.fill = true;
+    this.up = true;
+    this.down = true;
+    this.left = true;
+    this.right = true;
+  }
+  
+  show(){
+    if(this.y > 9){
+      fill(85, 237, 237);
+    } else if(this.y > 8){
+      fill(171, 255, 255);
+    } else if(this.y > 2){
+      fill(214, 255, 255);
+    } else{
+      fill(255, 255, 255);
+    }
+    if(this.fill){
+      rect(width / 18 * this.x, (height / 18 * this.y) + (3 * height / 18), width / 18, height / 18);
+    }
   }
 }
--- snowDigDug/snowDigDug.js
+++ snowDigDug/snowDigDug.js
@@ -3,22 +3,33 @@ var snowBlockArray;
 //https://i1.wp.com/www.edcollins.com/digdug/digdug-grid.gif image of dig dug game
 
 function setup() {
-  createCanvas(700,900);
+  createCanvas(900,900);
   snowflakeArray = [];
   snowBlockArray = [];
   for(let i = 0; i < 14;i++){
     snowBlockArray[i] = [];
-    for(let j = 0; i < 14;i++){
+    for(let j = 0; j < 14;j++){
       snowBlockArray[i][j] = new SnowBlock(i,j);
     }
   }
   
-  for(let i = 0; i < 14;i++){
-    snowflakeArray[i] = new Snowflake(random(0,width),0);
-  }
+  //for(let i = 0; i < 14;i++){
+   // snowflakeArray[i] = new Snowflake(random(0,width),0);
+  //}
 }
 
 
 function draw() {
+  background(31, 17, 120);
+  fill(0);
+  rect(14 * width / 18, 0, 4 * width / 18, height);
+  for(let i = 0; i < 14;i++){
+    for(let j = 0; j < 14;j++){
+      snowBlockArray[i][j].show();
+    }
+  }
   
+  //for(let i = 0; i < 14;i++){
+    //snowflakeArray[i].show();
+  //}
 }
