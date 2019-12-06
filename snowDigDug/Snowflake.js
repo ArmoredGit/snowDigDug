fbfff88b8e65a2f3ce6d365bef7576a19ab18d31
Miles Leska <37006041+ArmoredGit@users.noreply.github.com>
2019-12-06 07:06:08 -0600

    branches merge

--- snowDigDug/Snowflake.js
+++ snowDigDug/Snowflake.js
@@ -1,6 +1,47 @@
-class Snowflake {
-  constructor(x,y) {
-    this.x = x;
-    this.y = y;
+class SmallSnow {
+  constructor() {
+    this.x = random(900);
+    this.y = random(-20,-10);
+    this.speed=random(5)+3;
+    this.size=random(5);
+  }
+  
+  show(){
+    noStroke();
+    fill(255,255,255);
+    ellipse(this.x,this.y,this.size,this.size);
+  }
+  
+  move(){
+    this.y+=this.speed;
+    if (this.y>900){
+      this.y=-10;
+      this.x=random(900);
+      this.size=random(5);
+    }
+  }
+}
+
+class BigSnow {
+  constructor() {
+    this.x = random(900);
+    this.y = random(-20,-10);
+    this.speed=random(5)+3;
+    this.size=random(5)+5;
+  }
+  
+  show(){
+    noStroke();
+    fill(255,255,255);
+    ellipse(this.x,this.y,this.size,this.size);
+  }
+  
+  move(){
+    this.y+=this.speed;
+    if (this.y>900){
+      this.y=-10;
+      this.x=random(900);
+      this.size=random(5);
+    }
   }
 }
--- snowDigDug/snowDigDug.js
+++ snowDigDug/snowDigDug.js
@@ -2,18 +2,24 @@ var snowflakeArray;
 var snowBlockArray;
 var player1;
 //https://i1.wp.com/www.edcollins.com/digdug/digdug-grid.gif image of dig dug game
+//DigDug functions: http://www.edcollins.com/digdug/#:~:targetText=You%20score%20more%20points%20if,it%2C%20squashing%20it%20to%20death.
 
 function setup() {
   createCanvas(900,900);
   snowflakeArray = [];
   snowBlockArray = [];
+  for(let i = 0; i < 100;i++){
+    snowflakeArray[i]= new SmallSnow();
+  }
+  for(let i = 100; i < 200;i++){
+    snowflakeArray[i]= new BigSnow();
+  }
   for(let i = 0; i < 14;i++){
     snowBlockArray[i] = [];
     for(let j = 0; j < 14;j++){
       snowBlockArray[i][j] = new SnowBlock(i,j);
     }
   }
-  
   for(let i = 0; i < 6;i++){
     snowBlockArray[6][i].fill = false;
     snowBlockArray[6][i].up = false;
@@ -31,15 +37,12 @@ function setup() {
   snowBlockArray[5][6].right = false;
   
   player1 = new Player(6,6);
-  
-  //for(let i = 0; i < 14;i++){
-   // snowflakeArray[i] = new Snowflake(random(0,width),0);
-  //}
 }
 
 
 function draw() {
   background(31, 17, 120);
+  noStroke();
   fill(0);
   rect(14 * width / 18, 0, 4 * width / 18, height);
   for(let i = 0; i < 14;i++){
@@ -47,12 +50,11 @@ function draw() {
       snowBlockArray[i][j].show();
     }
   }
-  
+  for(let i = 0; i < 200;i++){
+    snowflakeArray[i].show();
+    snowflakeArray[i].move();
+  }
   player1.show();
-  
-  //for(let i = 0; i < 14;i++){
-    //snowflakeArray[i].show();
-  //}
 }
 
 function mouseDragged() {
@@ -66,5 +68,17 @@ function mouseDragged() {
     player1.move(2);
   } else if((pmouseX - mouseX) > width / 40 ){
     player1.move(4);
+  }
+}
+
+function keyPressed() {
+  if(keyCode==UP_ARROW){
+    
+  } else if (keyCode==DOWN_ARROW){
+    
+  } else if (keyCode==LEFT_ARROW){
+    
+  } else if (keyCode==RIGHT_ARROW){
+    
   }
 }
