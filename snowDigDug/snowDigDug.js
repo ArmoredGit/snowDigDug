0c095d2cdcf28ea511ef7215fd2baf2916d322ef
Miles Leska <37006041+ArmoredGit@users.noreply.github.com>
2019-12-07 11:03:56 -0600

    level 1 example

--- snowDigDug/snowDigDug.js
+++ snowDigDug/snowDigDug.js
@@ -1,7 +1,7 @@
 var snowflakeArray;
 var snowBlockArray;
 var player1;
-var testRock;
+var obs;
 //https://i1.wp.com/www.edcollins.com/digdug/digdug-grid.gif image of dig dug game
 //DigDug functions: http://www.edcollins.com/digdug/#:~:targetText=You%20score%20more%20points%20if,it%2C%20squashing%20it%20to%20death.
 
@@ -9,6 +9,7 @@ function setup() {
   createCanvas(900,900);
   snowflakeArray = [];
   snowBlockArray = [];
+  obs = [];
   for(let i = 0; i < 100;i++){
     snowflakeArray[i]= new SmallSnow();
   }
@@ -38,7 +39,66 @@ function setup() {
   snowBlockArray[5][6].right = false;
   
   player1 = new Player(6,6);
-  testRock = new Obstacle(null,7,7);
+  
+  
+  obs.push(new Obstacle("rock",4,2));
+  obs.push(new Obstacle("rock",10,8));
+  obs.push(new Obstacle("rock",3,10));
+  obs.push(new Obstacle("puf",1,2));
+  obs.push(new Obstacle("puf",10,1));
+  obs.push(new Obstacle("puf",9,10));
+  obs.push(new Obstacle("drg",4,9));
+  
+  snowBlockArray[1][1].fill = false;
+  snowBlockArray[1][1].down = false;
+  snowBlockArray[1][2].up = false;
+  snowBlockArray[1][2].fill = false;
+  snowBlockArray[1][2].down = false;
+  snowBlockArray[1][3].up = false;
+  snowBlockArray[1][3].fill = false;
+  snowBlockArray[1][3].down = false;
+  snowBlockArray[1][4].up = false;
+  snowBlockArray[1][4].fill = false;
+  snowBlockArray[1][4].down = false;
+  snowBlockArray[1][5].up = false;
+  snowBlockArray[1][5].fill = false;
+  
+  snowBlockArray[9][1].fill = false;
+  snowBlockArray[9][1].right = false;
+  snowBlockArray[10][1].left = false;
+  snowBlockArray[10][1].fill = false;
+  snowBlockArray[10][1].right = false;
+  snowBlockArray[11][1].left = false;
+  snowBlockArray[11][1].fill = false;
+  snowBlockArray[11][1].right = false;
+  snowBlockArray[12][1].left = false;
+  snowBlockArray[12][1].fill = false;
+  
+  snowBlockArray[2][9].fill = false;
+  snowBlockArray[2][9].right = false;
+  snowBlockArray[3][9].left = false;
+  snowBlockArray[3][9].fill = false;
+  snowBlockArray[3][9].right = false;
+  snowBlockArray[4][9].left = false;
+  snowBlockArray[4][9].fill = false;
+  snowBlockArray[4][9].right = false;
+  snowBlockArray[5][9].left = false;
+  snowBlockArray[5][9].fill = false;
+  
+  
+  snowBlockArray[9][8].fill = false;
+  snowBlockArray[9][8].down = false;
+  snowBlockArray[9][9].up = false;
+  snowBlockArray[9][9].fill = false;
+  snowBlockArray[9][9].down = false;
+  snowBlockArray[9][10].up = false;
+  snowBlockArray[9][10].fill = false;
+  snowBlockArray[9][10].down = false;
+  snowBlockArray[9][11].up = false;
+  snowBlockArray[9][11].fill = false;
+  snowBlockArray[9][11].down = false;
+  snowBlockArray[9][12].up = false;
+  snowBlockArray[9][12].fill = false;
 }
 
 
@@ -57,13 +117,10 @@ function draw() {
     }
   }
   player1.show();
-  testRock.show();
-  testRock.move();
+  obs.forEach(x => x.show());
 }
 
 function mouseDragged() {
-  //(mouseY - pmouseY)
-  //(mouseX - pmouseX)
   if((mouseY - pmouseY) > height / 40 ){
     player1.move(3);
   } else if((pmouseY - mouseY) > height / 40 ){
