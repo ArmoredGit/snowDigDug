de3765b4181f4942d2710351611c63c4e0bbf7f0
Miles Leska <37006041+ArmoredGit@users.noreply.github.com>
2019-12-13 08:37:51 -0600

    Rock drop kill points

--- snowDigDug/Obstacle.js
+++ snowDigDug/Obstacle.js
@@ -427,6 +427,22 @@ class Obstacle {
         if(!obs[i].equals(this.id)){
           if(dist(obs[i].x,obs[i].y,this.x,this.y) < 0.9 && this.special){
             obs.splice(i,1);
+            this.kills++;
+            if(round(this.kills)==1){
+              scoreBoard.add(1000);
+            } else if(round(this.kills)==2){
+              scoreBoard.add(2000);
+            } else if(round(this.kills)==3){
+              scoreBoard.add(4000);
+            } else if(round(this.kills)==4){
+              scoreBoard.add(6000);
+            } else if(round(this.kills)==5){
+              scoreBoard.add(8000);
+            } else if(round(this.kills)==6){
+              scoreBoard.add(10000);
+            } else if(round(this.kills)==7){
+              scoreBoard.add(12000);
+            }
           }
         }
       }
